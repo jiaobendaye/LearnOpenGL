73099d38e63a84a0b8a869d0ea970955d3debd25
jiaobendaye <240450828@qq.com>
2025-03-29 14:49:29 +0800

    change organize

--- xmake.lua
+++ xmake.lua
@@ -5,22 +5,4 @@ add_packages("glfw")
 
 includes("3rd/*/xmake.lua")
 
-for _, x in ipairs(os.files("hello/*.cpp")) do 
-    target(path.basename(x))
-        set_kind("binary")
-        add_deps("glad")
-        add_files(x)
-end
-
-target("myshader")
-    set_kind("binary")
-    add_deps("glad")
-    add_includedirs("myshader")
-    add_files("myshader/main.cpp")
-    after_build(function (target) 
-        os.cp(path.join("myshader/resource", "shader.*"), target:targetdir())
-    end)
-    on_clean(function (target) 
-        os.rm(path.join(target:targetdir(), "shader.*"))
-        os.rm(target:targetfile())
-    end)
+includes("hello")
