2f155acf1bf1cf7c3eefd926c324435826d505ce
jiaobendaye <240450828@qq.com>
2025-03-31 11:19:49 +0800

    add texture

--- hello/xmake.lua
+++ hello/xmake.lua
@@ -1,19 +1,29 @@
-for _, x in ipairs(os.files("*.cpp")) do 
+for _, x in ipairs(os.files("src/*.cpp")) do 
     target(path.basename(x))
         set_kind("binary")
         add_deps("glad")
         add_files(x)
 end
 
+
+target("resources_cp")
+    set_kind("phony")
+    after_build(function (target) 
+        os.cp(path.join("$(projectdir)/hello/resources/shaders", "*"), target:targetdir())
+        os.cp(path.join("$(projectdir)/hello/resources/textures", "*"), target:targetdir())
+    end)
+    after_clean(function (target) 
+        os.rm(path.join(target:targetdir(), "*"))
+    end)
+
 target("myshader")
     set_kind("binary")
     add_deps("glad")
-    add_includedirs("myshader")
-    add_files("myshader/main.cpp")
-    after_build(function (target) 
-        os.cp(path.join("myshader/resource", "shader.*"), target:targetdir())
-    end)
-    on_clean(function (target) 
-        os.rm(path.join(target:targetdir(), "shader.*"))
-        os.rm(target:targetfile())
-    end)
+    add_includedirs("include")
+    add_files("src/myshader/main.cpp")
+
+target("texture")
+    set_kind("binary")
+    add_deps("glad", "stb_image")
+    add_includedirs("include")
+    add_files("src/texture/main.cpp")
