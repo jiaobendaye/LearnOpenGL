f1f0ef7baf09904aa736ddcf4ef34d34d5534449
jiaobendaye <240450828@qq.com>
2025-04-01 14:41:48 +0800

    change organize

--- hello/xmake.lua
+++ hello/xmake.lua
@@ -1,13 +1,19 @@
+set_group("hello")
+
 for _, x in ipairs(os.files("src/*.cpp")) do 
     target(path.basename(x))
         set_kind("binary")
-        add_deps("glad")
+        add_deps("glad", "stb_image")
+        add_includedirs("include")
         add_files(x)
 end
 
 
-target("resources_cp")
+target("resources")
     set_kind("phony")
+    for _, x in ipairs(os.files("src/*.cpp")) do 
+        add_deps(path.basename(x))
+    end
     after_build(function (target) 
         os.cp(path.join("$(projectdir)/hello/resources/shaders", "*"), target:targetdir())
         os.cp(path.join("$(projectdir)/hello/resources/textures", "*"), target:targetdir())
@@ -15,15 +21,3 @@ target("resources_cp")
     after_clean(function (target) 
         os.rm(path.join(target:targetdir(), "*"))
     end)
-
-target("myshader")
-    set_kind("binary")
-    add_deps("glad")
-    add_includedirs("include")
-    add_files("src/myshader/main.cpp")
-
-target("texture")
-    set_kind("binary")
-    add_deps("glad", "stb_image")
-    add_includedirs("include")
-    add_files("src/texture/main.cpp")
