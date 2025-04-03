859f3dfb243bb33f34ab2950897eeffc3585fdaa
jiaobendaye <240450828@qq.com>
2025-04-03 11:02:54 +0800

    add camera

--- hello/xmake.lua
+++ hello/xmake.lua
@@ -8,18 +8,3 @@ for _, x in ipairs(os.files("src/*.cpp")) do
         set_rundir("$(projectdir)/hello")
         add_files(x)
 end
-
-
-target("resources")
-    set_kind("phony")
-    set_default(false)
-    on_build(function (target) 
-        if (not os.isdir(target:targetdir())) then
-            os.mkdir(target:targetdir())
-        end
-        os.cp(path.join("$(projectdir)/hello/resources/shaders", "*"), target:targetdir())
-        os.cp(path.join("$(projectdir)/hello/resources/textures", "*"), target:targetdir())
-    end)
-    after_clean(function (target) 
-        os.rm(path.join(target:targetdir(), "*"))
-    end)
