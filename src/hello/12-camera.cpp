95666b570e98defeed1461365f067912319f2453
jiaobendaye <240450828@qq.com>
2025-04-08 15:32:25 +0800

    fix window build

--- src/hello/12-camera.cpp
+++ src/hello/12-camera.cpp
@@ -21,8 +21,8 @@ glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 
-float deltaTime = 0.0f; // 当前帧与上一帧的时间差
-float lastFrame = 0.0f; // 上一帧的时间
+float deltaTime = 0.0f; 
+float lastFrame = 0.0f; 
 
 int main()
 {
