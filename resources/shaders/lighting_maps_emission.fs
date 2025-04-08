0a0ca9ae23d6c988b0b3a5fe4ce73da744f81883
jiaobendaye <240450828@qq.com>
2025-04-08 15:04:49 +0800

    add lighting maps emission

--- resources/shaders/lighting_maps_emission.fs
+++ resources/shaders/lighting_maps_emission.fs
@@ -4,6 +4,7 @@ out vec4 FragColor;
 struct Material {
     sampler2D diffuse;
     sampler2D specular;    
+    sampler2D emission;
     float shininess;
 }; 
 
@@ -39,8 +40,10 @@ void main()
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
-    // vec3 specular = light.specular * spec * (vec3(1.0) - texture(material.specular, TexCoords).rgb); // here we inverse the sampled specular color. Black becomes white and white becomes black.
+    
+      // emission
+    vec3 emission = texture(material.emission, TexCoords).rgb;
         
-    vec3 result = ambient + diffuse + specular;
+    vec3 result = ambient + diffuse + specular + emission;
     FragColor = vec4(result, 1.0);
 } 
