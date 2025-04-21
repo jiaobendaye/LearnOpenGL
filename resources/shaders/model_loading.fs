3f94b8dc00d26ed9cafc0769c2cf3fef06c04c14
fanzh <240450828@qq.com>
2025-04-21 20:12:49 +0800

    add stencil_testing

--- resources/shaders/model_loading.fs
+++ resources/shaders/model_loading.fs
@@ -5,7 +5,21 @@ in vec2 TexCoord;
 
 uniform sampler2D texture_diffuse1;
 
+float near = 0.1; 
+float far  = 100.0; 
+
+float LinearizeDepth(float depth) 
+{
+    float z = depth * 2.0 - 1.0; // 转换为 NDC
+    return (2.0 * near * far) / (far + near - z * (far - near));    
+}
+
 void main()
 {    
     FragColor = texture(texture_diffuse1, TexCoord);
+    // FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+
+    // float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 far
+    // FragColor = vec4(vec3(depth), 1.0);
+
 }
